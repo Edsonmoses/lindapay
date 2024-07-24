7f7462a77fd6bc749e022d531bb54d6d66c2bb06
Edson Wandera <edsonwandera@yahoo.com>
2024-07-24 13:45:21 +0300

    Delete src directory

--- src/components/index.js
+++
@@ -1,7 +0,0 @@
-export {default as Navbar} from './Navbar';
-export {default as Footer} from './Footer';
-export {default as ProductListing} from './ProductListing';
-export {default as Testimonial} from './Testimonial';
-export {default as Partners} from './Partners';
-export {default as Faqs} from './Faqs';
-export {default as Banner} from './Banner';
